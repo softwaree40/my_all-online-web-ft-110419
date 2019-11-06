758f87f8df7fb2ed9fa3c436ed7eafd2eb5beda3
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 14:32:16 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,6 @@ require 'pry'
 def my_all?(collection)
      new_array = []
     collection.each do |collect|
-      if collect < 3
-        return false
-      end
       new_array << yield(collect)
      #binding.pry
     end
