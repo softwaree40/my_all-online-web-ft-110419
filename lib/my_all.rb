558f464966d199560f675b78ddc68b2e43f04a2f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 14:09:03 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,5 +6,10 @@ def my_all?(collection)
       new_array << yield(collect)
    #binding.pry
    end
+     if collect < 3
+       return false
+     else
+       return true
+     end
    new_array
 end
